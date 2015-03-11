1f1c297b116bfa48ac974140ac0e96493d383a02
TrinhTV3 <trinhtv060392@gmail.com>
2015-03-11 14:24:42 +0700

    create sample about fill, findindex, last API in lodash

--- app/js/myjs.js
+++ app/js/myjs.js
@@ -21,7 +21,43 @@ console.log(_.pluck(_.dropRightWhile(users, { 'user': 'pebbles', 'active': false
 
 console.log(_.pluck(_.dropRightWhile(users, 'active', false), 'user'));
 
-console.log(_.pluck(_.dropRightWhile(users, 'active1', false), 'user'));
-
+console.log(_.dropRightWhile(users, 'active1', false), 'user');
+console.log(_.first(users));
 var fill = [1,2,3,4,5];
-console.log(_.fill(fill,4,1,fill.length));
+console.log(_.fill(fill,4,1,3));
+console.log(fill.length);
+
+var test = [{'a': 'b'},{'a':'c'}];
+console.log(_.pluck(test,'a'));
+
+
+var flatten = [1,[3,2,[4,5,5,6,[4]]]];
+
+console.log(_.flatten(flatten));
+console.log(_.flatten(flatten,true));
+console.log(_.flattenDeep(flatten));
+
+var init = _.flattenDeep(flatten);
+console.log(_.initial(init));
+console.log(init);
+console.log(_.initial([1,2,3]));
+
+console.log(_.intersection(init));
+
+console.log(_.flattenDeep(_.intersection([1, 2], [4, 2], [2, 1])));
+
+console.log(_.intersection([2,2,1],[,4,2],[4,8,2]));
+
+console.log(_.lastIndexOf([1,1,2,2,2,2,2,2,3,4],2,true));
+console.log(_.indexOf([1,1,2,2,2,2,2,2,3,4],2,true));
+
+var array = [5, 10, 15, 20];
+var evens = _.pullAt(array, 1, 3);
+
+console.log(array);
+// → [5, 15]
+
+console.log(evens);
+// → [10, 20]
+
+
